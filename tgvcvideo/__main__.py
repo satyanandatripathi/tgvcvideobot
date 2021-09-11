61a29119a5571baf0c537e3aea611b7e73a49ce3
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-09-11 15:01:10 +0530

    Hihihihi

--- tgvcvideo/__main__.py
+++ tgvcvideo/__main__.py
@@ -3,8 +3,8 @@
 import asyncio
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
-from bot.videoplayer import app
-from bot.videoplayer import call_py
+from tgvcvideo.tgvcvideoplayer import app
+from tgvcvideo.tgvcvideoplayer import call_py
 
 bot = Client(
     ":memory:",
