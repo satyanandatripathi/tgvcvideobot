c8d8163186a2ebc51823e03523ac863baa754196
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-09-11 14:59:52 +0530

    Tapa tap

---
+++ tgvcvideo/__main__.py
@@ -0,0 +1,24 @@
+# Surprise MF
+
+import asyncio
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from bot.videoplayer import app
+from bot.videoplayer import call_py
+
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="bot"),
+)
+
+bot.start()
+print("[STATUS]:✅ »» BOT CLIENT STARTED ««")
+app.start()
+print("[STATUS]:✅ »» USERBOT CLIENT STARTED ««")
+call_py.start()
+print("[STATUS]:✅ »» PYTGCALLS CLIENT STARTED ««")
+idle()
+print("[STATUS]:❌ »» BOT STOPPED ««")
